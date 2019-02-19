adb3a127a69f452c370ba2af717e72fda3a5a4b8
Pavol Risa <risapav@gmail.com>
2019-02-19 22:00:15 +0100

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,56 @@
 #include <Arduino.h>
 
+#define PIN_LED 13
+
+#ifdef ARDUINO_OTA
+
+#include <ArduinoOTA.h>
+
+void setupOTA() {
+
+    ArduinoOTA.setPort(OTA_PORT);
+    ArduinoOTA.setHostname(HOSTNAME);
+
+    ArduinoOTA.onStart([]() { Serial.println("Starting"); });
+    ArduinoOTA.onEnd([]() { Serial.println("\nEnd"); });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+
+    ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) {
+            Serial.println("Auth Failed");
+        } else if (error == OTA_BEGIN_ERROR) {
+            Serial.println("Begin Failed");
+        } else if (error == OTA_CONNECT_ERROR) {
+            Serial.println("Connect Failed");
+        } else if (error == OTA_RECEIVE_ERROR) {
+            Serial.println("Receive Failed");
+        } else if (error == OTA_END_ERROR) {
+            Serial.println("End Failed");
+        }
+    });
+    ArduinoOTA.begin();
+}
+
+#endif
+
 void setup() {
-  // put your setup code here, to run once:
+    // Setting up heartbeat
+    pinMode(PIN_LED, OUTPUT);
+    // Other setup code
+#ifdef ARDUINO_OTA
+    setupOTA();
+#endif
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // Other loop code
+#ifdef ARDUINO_OTA
+    ArduinoOTA.handle();
+#endif
+    // Heartbeat
+    digitalWrite(PIN_LED, !digitalRead(PIN_LED));
+    delay(250);
 }
